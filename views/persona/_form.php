c7798baf01f1468973a1b8ab90f4810a02e849d7
Norman Salvador Aráuz López <netman.arauz@gmail.com>
2018-04-10 09:59:13 -0600

    Corrección de la vista
    Se corrigió el formulario de persona create

--- views/persona/_form.php
+++ views/persona/_form.php
@@ -18,44 +18,50 @@ use yii\helpers\ArrayHelper;
          'validationUrl'=>Url::toRoute('persona/validation'),
         ]); ?>
 
-    <div class="alert alert-success">
-        Información personal
+    <div class="panel panel-success">
+        <div class="panel-heading">
+            <h3 class="panel-title">Datos Personales</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-2">
+                    <?= $form->field($model, 'Nombres')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-2">
+                    <?= $form->field($model, 'Apellidos')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-2">
+                    <?= $form->field($model, 'Cedula')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-2">
+                    <?= $form->field($model, 'Telefono')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-2">
+                    <?= $form->field($model, 'IdTipo')->dropDownList(
+                        ArrayHelper::map(TipoPersona::find()->asArray()->all(),'Id', 'Descripcion'),
+                        ['prompt'=>'Seleccione Persona','required'=>true]
+                    );?>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="panel panel-success">
+        <div class="panel-heading">
+            <h3 class="panel-title">Credenciales de Acceso</h3>
+        </div>
+        <div class="panel-body">
 
-    <div class="row">
-        <div class="col-md-2">
-            <?= $form->field($model, 'Nombres')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-2">
-            <?= $form->field($model, 'Apellidos')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-2">
-            <?= $form->field($model, 'Cedula')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-2">
-            <?= $form->field($model, 'Telefono')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-2">
-            <?= $form->field($model, 'IdTipo')->dropDownList(
-                ArrayHelper::map(TipoPersona::find()->asArray()->all(),'Id','Descripcion'),
-                ['prompt'=>'Seleccione el Tipo', 'required'=>true]
-            );?>
-        </div>
-    </div>
-
-    <div class="alert alert-success">
-        Credenciales de Acceso
-    </div>
-
-    <div class="row">
-        <div class="col-md-3">
-            <?= $form->field($user, 'email')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-3">
-            <?= $form->field($user, 'username')->textInput(['maxlength' => true, 'required'=>true]) ?>
-        </div>
-        <div class="col-md-3">
-            <?= $form->field($user, 'password')->passwordInput(['required'=>true]) ?>
+            <div class="row">
+                <div class="col-md-3">
+                    <?= $form->field($user, 'email')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-3">
+                    <?= $form->field($user, 'username')->textInput(['maxlength' => true, 'required'=>true]) ?>
+                </div>
+                <div class="col-md-3">
+                        <?= $form->field($user, 'password')->passwordInput(['required'=>true]) ?>
+                </div>
+            </div>
         </div>
     </div>
 
